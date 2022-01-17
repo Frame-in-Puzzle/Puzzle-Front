df10462864b42521d2725ff0eefee683b8d88002
JeongTaehwan <wjdxoghks11222@naver.com>
2022-01-17 22:10:02 +0900

    Update : 요청사항 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import GlobalStyle from "./Styles/GlobalStyle";
 import { Global, ThemeProvider } from "@emotion/react";
-
-const theme = {
-  buttons: {
-    Big: 'width: 280px; height: 90px',
-    Medium: 'width: 150px; height: 60px',
-    Regular: 'width: 100px; height: 40px',
-    Small: 'width: 90px; height: 30px',
-  },
-  fonts: {
-    h1: '78px',
-    h2: '60px',
-    h3: '37px',
-    h4: '25px',
-    h5: '20px',
-    h6: '13px',
-  }
-}
+import {theme} from './Styles/theme';
 
 function App() {
   return <ThemeProvider theme={theme}>
---
+++ src/Styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+    buttons: {
+      Big: 'width: 280px; height: 90px;',
+      Medium: 'width: 150px; height: 60px;',
+      Regular: 'width: 100px; height: 40px;',
+      Small: 'width: 90px; height: 30px;',
+    },
+    fonts: {
+      h1: '78px',
+      h2: '60px',
+      h3: '37px',
+      h4: '25px',
+      h5: '20px',
+      h6: '13px',
+    }
+  }
