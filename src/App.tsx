f270a94e6f151af4b7e37bcb630388478cdb630e
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-01-17 21:26:12 +0900

    Update : Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import GlobalStyle from "./Styles/GlobalStyle";
 import { Global } from "@emotion/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Global styles={GlobalStyle} />
-  </div>;
+  return (
+    <BrowserRouter>
+      <Global styles={GlobalStyle} />
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
