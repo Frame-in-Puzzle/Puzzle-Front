bd004ebbb15172a01cb67ad11499b6b70b72b7eb
eodn <s20063@gsm.hs.kr>
2022-01-17 18:51:37 +0900

    Add : GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
+import GlobalStyle from "./Styles/GlobalStyle";
+import { Global } from "@emotion/react";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Global styles={GlobalStyle} />
+  </div>;
 }
 
 export default App;
---
+++ src/Styles/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+const GlobalStyle = css`
+ * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+  body {
+    width: 100%;
+    height: 100vh;
+  }`;
+
+  export default GlobalStyle;
