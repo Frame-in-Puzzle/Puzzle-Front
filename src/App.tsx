9226ca3cd771521cc48f1fce034b559750d34d1b
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-01-17 22:58:00 +0900

    Update : 절대경로 설정 했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import GlobalStyle from "./Styles/GlobalStyle";
+import GlobalStyle from "Styles/GlobalStyle";
 import { Global } from "@emotion/react";
 
 function App() {
