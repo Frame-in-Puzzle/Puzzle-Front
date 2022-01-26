1a6188eaf22eb517080b76ddf107cad8d08d3c00
JeongTaehwan <wjdxoghks11222@naver.com>
2022-01-26 17:33:29 +0900

    Update : Mainpage 스타일 제거

--- src/Pages/Mainpage/Mainpage.tsx
+++ src/Pages/Mainpage/Mainpage.tsx
@@ -1,17 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
-import * as S from "./Style";
 import { Header } from "../../Components";
 import Banner from "../../Components/Common/Banner/Banner";
 import Post from "../../Components/Post/Post";
 
 const Mainpage = () => {
   return (
-    <div css={S.Positioner}>
+    <>
       <Header />
       <Banner />
       <Post />
-    </div>
+    </>
   );
 };
 
--- src/Pages/Mainpage/Style.ts
+++
@@ -1,6 +0,0 @@
-import { css } from "@emotion/react";
-import { theme } from "../../Styles/theme";
-
-export const Positioner = css``;
-
-export const Container = css``;
