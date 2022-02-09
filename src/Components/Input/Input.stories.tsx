c84c42d56e17144d7d8d0a7727a9368028e75a92
JeongTaehwan <wjdxoghks11222@naver.com>
2022-02-09 14:44:31 +0900

    Add : Input 컴포넌트 추가

---
+++ src/Components/Input/Input.stories.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Input from "./Input";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+  title: "Components/Input",
+  components: Input,
+  argTypes: {
+    type: {
+      name: "type",
+      type: {
+        name: "enum",
+        value: [
+          "text",
+          "password",
+          "date",
+          "month",
+          "checkbox",
+          "email",
+          "file",
+          "hidden",
+        ],
+      },
+    },
+    theme: {
+      name: "theme",
+      type: {
+        name: "enum",
+        value: [
+          "ProfileRegistrationPageInput",
+          "WritePageInput",
+          "ProfileModifyPageInput",
+        ],
+      },
+      defaultValue: "BlackButtonWithShadow",
+    },
+    fontSize: {
+      name: "fontSize",
+      type: {
+        name: "enum",
+        value: ["h6", "h5", "h4", "h3", "h2", "h1"],
+      },
+    },
+    placeholder: {
+      name: "placeholder",
+      type: { name: "string", required: false },
+      defaultValue: "",
+    },
+  },
+} as ComponentMeta<typeof Input>;
+
+export const input: ComponentStory<typeof Input> = (props) => (
+  <Input
+    type={props.type}
+    placeholder={props.placeholder}
+    theme={props.theme}
+    fontSize={props.fontSize}
+  />
+);
