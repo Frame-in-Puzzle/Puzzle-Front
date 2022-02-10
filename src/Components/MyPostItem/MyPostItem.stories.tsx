18b15938bdb7479be861e1958d26b99f6c5e8a27
eodn <s20063@gsm.hs.kr>
2022-02-10 20:53:34 +0900

    fix : 더미 데이터 수정

--- src/Components/MyPostItem/MyPostItem.stories.tsx
+++ src/Components/MyPostItem/MyPostItem.stories.tsx
@@ -7,6 +7,15 @@ export default {
   components: MyPostItem,
 } as ComponentMeta<typeof MyPostItem>;
 
+const MyPostObj = {
+  id: 1,
+  category: "WEB",
+  title: "PUZZLE",
+  content:
+    "가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ가아ㅓ라어랑러아ㅓㄹ나어래저머햐ㅐㅓㅇ랴ㅐㅓ야ㅐ",
+  state: "모집중",
+};
+
 export const mypostitem: ComponentStory<typeof MyPostItem> = ({
   MyPostObj,
 }) => <MyPostItem MyPostObj={MyPostObj} />;
