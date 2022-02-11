df74a8b45a7e7390ebc94838dabbd5293ae47bd7
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-02-11 20:08:32 +0900

    Update : Header 변경

--- src/Components/Common/Header/Style.ts
+++ src/Components/Common/Header/Style.ts
@@ -10,23 +10,37 @@ export const Positioner = css`
   justify-content: space-between;
 `;
 
-export const RightContainer = css`
-  width: 200px;
-  font-size: ${theme.fonts.h5};
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  img {
-    width: 60px;
-    height: 60px;
-    border-radius: 50%;
-    box-shadow: rgb(0 0 0 / 9%) 0px 0px 8px;
-  }
-`;
+export const RightContainer = {
+  Login: css`
+    width: 200px;
+    font-size: ${theme.fonts.h5};
+    display: flex;
+    justify-content: space-between;
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      box-shadow: rgb(0 0 0 / 9%) 0px 0px 8px;
+    }
+  `,
+  NoneLogin: css``,
+  Write: css`
+    font-size: ${theme.fonts.h5};
+    display: flex;
+    justify-content: space-between;
+    img {
+      width: 60px;
+      height: 60px;
+      border-radius: 50%;
+      box-shadow: rgb(0 0 0 / 9%) 0px 0px 8px;
+    }
+  `,
+};
 
 export const ProfileWrapper = css`
   width: 85px;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  cursor: pointer;
 `;
