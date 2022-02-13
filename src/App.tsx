9fb92ad9f1d488ecf2a307f7c1a98a6cb9363a7d
eodn <s20063@gsm.hs.kr>
2022-02-13 17:55:05 +0900

    fix : comflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import GlobalStyle from "./Styles/GlobalStyle";
 import { Global, ThemeProvider } from "@emotion/react";
 import { theme } from "./Styles/theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import Mainpage from "./Pages/Mainpage/Mainpage";
 import { ProfileCheckPage } from "./Pages";
 import Profilepage from "./Pages/Profilepage/Profilepage";
 import TestPage from "./Pages/TestPage/TestPage";
+import WritePage from "./Pages/WritePage/WritePage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Global styles={GlobalStyle} />
-        <Routes>
-          <Route path="/" element={<TestPage />} />
-          <Route path="/main" element={<Mainpage />} />
-          <Route path="/profile/check" element={<ProfileCheckPage />} />
-          <Route path="/profile" element={<Profilepage />} />
-        </Routes>
-      </ThemeProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <Global styles={GlobalStyle} />
+          <Routes>
+            <Route path="/" element={<TestPage />} />
+            <Route path="/main" element={<Mainpage />} />
+            <Route path="/profile/check" element={<ProfileCheckPage />} />
+            <Route path="/profile" element={<Profilepage />} />
+            <Route path="/write" element={<WritePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 }
 
