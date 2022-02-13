b3fbf8546063b34b4516c17a484b48aa8cb8df5a
eodn <s20063@gsm.hs.kr>
2022-02-13 17:57:22 +0900

    fix : conflict

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,9 +1,6 @@
 export { default as Header } from "./Common/Header/Header";
-<<<<<<< HEAD
 export { default as MyPost } from "./MyPost/MyPost";
 export { default as MyPostItem } from "./MyPostItem/MyPostItem";
-export { default as ProfileHeader } from "./ProfileHeader/ProfileHeader";
-=======
 export { default as Post } from "./Post/Post";
 export { default as PostItem } from "./PostItem/PostItem";
 export { default as Input } from "./Input/Input";
@@ -16,4 +13,3 @@ export { default as WriteTextInput } from "./Write/WriteTextInput/WriteTextInput
 export { default as DropDown } from "./DropDown/DropDown";
 export { default as DropDownList } from "./DropDownList/DropDownList";
 export { default as Button } from "./Button/Button";
->>>>>>> e43df25bd513efdc80d02e201fb343959c707cc9
