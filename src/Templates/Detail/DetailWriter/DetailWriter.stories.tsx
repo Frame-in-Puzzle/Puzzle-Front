615c33880d4f9195ede5bbce587fe91f5207ddf0
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-02-16 22:06:52 +0900

    Add : DetailWriter 추가

---
+++ src/Templates/Detail/DetailWriter/DetailWriter.stories.tsx
@@ -0,0 +1,17 @@
+import DetailWriter from "./DetailWriter";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+const writerObj = {
+  image: "https://avatars.githubusercontent.com/u/66630940?v=4",
+  name: "ImChangGyu",
+  description: "Front-End Developer가 되고싶습니다.",
+};
+
+export default {
+  title: "Templates/Detail/DetailWriter",
+  component: DetailWriter,
+} as ComponentMeta<typeof DetailWriter>;
+
+export const detailWriter: ComponentStory<typeof DetailWriter> = () => (
+  <DetailWriter writerObj={writerObj} />
+);
