22d76808b0c37f72498c819427b36dee5f63354b
eodn <s20063@gsm.hs.kr>
2022-03-10 15:49:29 +0900

    fix : signinmodal 수정

--- src/Components/SigInModal/SiginModal.tsx
+++ src/Components/SigInModal/SiginModal.tsx
@@ -1,17 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./Style";
 import * as I from "../../Assets";
 import { FiX } from "react-icons/fi";
 import { FaGithubSquare } from "react-icons/fa";
 
 interface ModalProps {
-  modalState: boolean;
   closeModal: (e: any) => void;
 }
 
-const SigInModal: React.FC<ModalProps> = ({ modalState, closeModal }) => {
-  return modalState ? (
+const SigInModal: React.FC<ModalProps> = ({ closeModal }) => {
+  return (
     <div css={S.Positioner}>
       <div css={S.Overay} onClick={(e) => closeModal(e)} />
       <div css={S.Container}>
@@ -23,15 +22,13 @@ const SigInModal: React.FC<ModalProps> = ({ modalState, closeModal }) => {
         </div>
         <div css={S.Content}>
           <h1>PUZZLE 에 오신 것을 환영합니다!</h1>
-          <div css={S.GithubContent}>
+          <a href={process.env.REACT_APP_OAUTH_URL} css={S.GithubContent}>
             <FaGithubSquare css={S.GithubIcon} />
             <h2>Github 로그인</h2>
-          </div>
+          </a>
         </div>
       </div>
     </div>
-  ) : (
-    <></>
   );
 };
 
