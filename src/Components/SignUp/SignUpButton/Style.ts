be6e6bc3c9726c01c2fed71680620a44e23e610d
eodn <s20063@gsm.hs.kr>
2022-03-18 15:31:19 +0900

    fix : 코드 삭제

--- src/Components/SignUp/SignUpButton/Style.ts
+++
@@ -1,8 +0,0 @@
-import { css } from "@emotion/react";
-import { theme } from "../../../Styles/theme";
-
-export const Positioner = css`
-  width: 300px;
-  display: flex;
-  justify-content: space-between;
-`;
