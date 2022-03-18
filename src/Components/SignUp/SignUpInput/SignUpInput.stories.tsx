f8b14f26b3c3631c62d3a61b8d484616c0381927
eodn <s20063@gsm.hs.kr>
2022-03-18 11:56:53 +0900

    add : storybook useState 추가

--- src/Components/SignUp/SignUpInput/SignUpInput.stories.tsx
+++ src/Components/SignUp/SignUpInput/SignUpInput.stories.tsx
@@ -1,11 +1,20 @@
 import SignInput from "./SignUpInput";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useState } from "react";
 
 export default {
   title: "Components/SignUp",
   components: SignInput,
 } as ComponentMeta<typeof SignInput>;
 
+const [githubIdChanged, setGithubIdChanged] = useState(false);
+const [emailChanged, setEmailChanged] = useState(false);
+const [bioChanged, setBioChanged] = useState(false);
+
 export const signupinput: ComponentStory<typeof SignInput> = () => (
-  <SignInput />
+  <SignInput
+    setGithubIdChanged={setGithubIdChanged}
+    setEmailChanged={setEmailChanged}
+    setBioChanged={setBioChanged}
+  />
 );
