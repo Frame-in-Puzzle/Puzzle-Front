8fb03c482b0570ce3699ebaa1a6e180ed3291a18
eodn <s20063@gsm.hs.kr>
2022-03-18 11:59:29 +0900

    add : useState 추가

--- src/Pages/SignUpPage/SignUpPage.tsx
+++ src/Pages/SignUpPage/SignUpPage.tsx
@@ -1,14 +1,26 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useState } from "react";
 import { SignUpButton, SignUpInput, SignUpHeader } from "../../Components";
 import * as S from "./Style";
 
 const SignUpPage = () => {
+  const [githubIdChanged, setGithubIdChanged] = useState(false);
+  const [emailChanged, setEmailChanged] = useState(false);
+  const [bioChanged, setBioChanged] = useState(false);
+
   return (
     <div css={S.Positioner}>
       <SignUpHeader />
-      <SignUpInput />
-      <SignUpButton />
+      <SignUpInput
+        setGithubIdChanged={setGithubIdChanged}
+        setEmailChanged={setEmailChanged}
+        setBioChanged={setBioChanged}
+      />
+      <SignUpButton
+        githubIdChanged={githubIdChanged}
+        emailChanged={emailChanged}
+        bioChanged={bioChanged}
+      />
     </div>
   );
 };
