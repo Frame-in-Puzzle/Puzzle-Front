49f8033a6e2e5b4bf470d26eda566c913583b72b
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-03-22 18:04:59 +0900

    Add : Tag Selector 추가

---
+++ src/Templates/Tag/TagSelector.stories.tsx
@@ -0,0 +1,11 @@
+import TagSelector from "./TagSelector";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "Templates/Tag/TagSelector",
+  component: TagSelector,
+} as ComponentMeta<typeof TagSelector>;
+
+export const tagSelector: ComponentStory<typeof TagSelector> = () => (
+  <TagSelector />
+);
---
+++ src/Templates/Tag/TagSelector.tsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+import React, { useState } from "react";
+import { TagModal } from "../../Components";
+import { useRecoilValue } from "recoil";
+import {
+  fieldSelected,
+  languageSelected,
+  purposeSelected,
+} from "../../Atoms/AtomContainer";
+import * as S from "./Style";
+
+const TagSelector = () => {
+  const [tagModalState, setTagModalState] = useState(false);
+  const languageSelect = useRecoilValue(languageSelected);
+  const fieldSelect = useRecoilValue(fieldSelected);
+  const purposeSelect = useRecoilValue(purposeSelected);
+
+  const closeTagModal = () => {
+    setTagModalState(false);
+  };
+
+  return (
+    <>
+      <div css={S.Positioner} onClick={() => setTagModalState(true)}>
+        <span css={S.Purpose}>{purposeSelect}</span>
+        <span css={S.Field}>{fieldSelect.join(" | ")}</span>
+        {fieldSelect.includes("전체") ? "" : ">"}
+        <span css={S.Language}>{languageSelect.join(" | ")}</span>
+      </div>
+      {tagModalState && <TagModal closeTagModal={closeTagModal} />}
+    </>
+  );
+};
+
+export default TagSelector;
