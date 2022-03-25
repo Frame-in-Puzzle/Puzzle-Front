11c2c5cdec49f777915e67da41c95583c250495f
eodn <s20063@gsm.hs.kr>
2022-03-25 15:04:49 +0900

    add : 로고 navigate, 프로필 이미지 aip get

--- src/Components/Common/Header/Header.tsx
+++ src/Components/Common/Header/Header.tsx
@@ -1,7 +1,10 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from "./Style";
 import * as I from "../../../Assets";
+import { useDecode } from "../../../Hooks/useDecode";
+import { getUser } from "../../../Lib/Api/member/member";
+import { useNavigate } from "react-router";
 
 interface HeaderProps {
   theme: "NoneLogin" | "Login" | "Write";
@@ -9,9 +12,19 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, theme }) => {
+  const { sub } = useDecode();
+  const [imageUrl, setImageUrl] = useState<string>("");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUser(sub).then((res) => {
+      setImageUrl(res.data.imageUrl);
+    });
+  }, []);
   return (
     <div css={S.Positioner}>
-      <I.PuzzleLogo />
+      <I.PuzzleLogo onClick={() => navigate("/main")}></I.PuzzleLogo>
       <div css={[S.RightContainer[theme]]}>{children}</div>
     </div>
   );
