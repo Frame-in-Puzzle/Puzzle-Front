5d5e5f0c8503f18414ef68ff33eba578e21f8531
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-04-02 14:24:55 +0900

    Update : 신청 목록 불러오기

--- src/Templates/Detail/DetailApply/DetailApply.tsx
+++ src/Templates/Detail/DetailApply/DetailApply.tsx
@@ -2,31 +2,46 @@
 import React from "react";
 import * as S from "./Style";
 import { ApplyItem } from "../../../Components";
+import useSWR from "swr";
+import { apiClient } from "../../../Lib/Api/apiClient";
 
-type DetailApply = {
-  Image: string;
-  name: string;
-  tag: string[];
-  date: string;
-};
-
-interface DetailApplyProps {
-  apply: number;
-  applyObj: DetailApply[];
+interface attendProps {
+  data: [
+    {
+      attendStatus: string;
+      githubId: string;
+      id: number;
+      imageUrl: string;
+      languages: string[];
+      name: string;
+    },
+  ];
 }
 
-const DetailApply: React.FC<DetailApplyProps> = ({ apply, applyObj }) => {
+interface DetailApplyProps {
+  board_idx: number;
+  writer: string;
+}
+
+const DetailApply: React.FC<DetailApplyProps> = ({ board_idx, writer }) => {
+  const { data: attend, error: attendError } = useSWR<attendProps>(
+    `/attend/board/${board_idx}`,
+    apiClient.get,
+  );
+
+  if (!attend) return <div />;
+  if (attendError) return <div />;
   return (
     <div css={S.Positioner}>
       <div css={S.ApplyContainer}>
-        <span css={S.Apply}>{apply}개의 신청</span>
+        <span css={S.Apply}>{attend.data.length}개의 신청</span>
         <span css={S.ApplyDescription}>
           수락 또는 거절 후 취소가 불가합니다.
         </span>
       </div>
       <div>
-        {applyObj.map((item, key) => (
-          <ApplyItem applyObj={item} key={key} />
+        {attend.data.map((attend, idx) => (
+          <ApplyItem attend={attend} writer={writer} key={idx} />
         ))}
       </div>
     </div>
