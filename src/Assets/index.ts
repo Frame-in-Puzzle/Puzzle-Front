f195de9672d02f698e5b73e0c2c0703b52024a4b
eodn <s20063@gsm.hs.kr>
2022-04-05 16:08:21 +0900

    add : 이미지 추가

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -8,3 +8,5 @@ export { default as BurgerMenu } from "./BurgerMenu";
 export { default as MyPostDefaultImg } from "./MyPostDefaultImg";
 export { default as SigninModalImg } from "./SigninModalImg";
 export { default as SignUpHeaderImg } from "./SignUpHeaderImg";
+export { default as PromotionImg1 } from "./PromotionImg1";
+export { default as RoketImg } from "./RoketImg";
