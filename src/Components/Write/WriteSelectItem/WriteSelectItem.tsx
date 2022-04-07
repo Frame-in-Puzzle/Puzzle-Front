a6be0efe2ee62a372831935e4c6ed99833095304
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-04-07 17:12:53 +0900

    Update : props 추가

--- src/Components/Write/WriteSelectItem/WriteSelectItem.tsx
+++ src/Components/Write/WriteSelectItem/WriteSelectItem.tsx
@@ -10,7 +10,7 @@ const WriteSelectItem = () => {
       <label css={S.Text}>
         <p>프로젝트 :</p>
       </label>
-      <TagSelector />
+      <TagSelector onSubmit={function (): void {}} />
     </div>
   );
 };
