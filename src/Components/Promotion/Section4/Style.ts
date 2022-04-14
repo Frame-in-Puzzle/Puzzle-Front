e18dc8f26f365e9c5324d1e4b0dabcbcb986e718
eodn <s20063@gsm.hs.kr>
2022-04-14 21:08:51 +0900

    add : Section4 완료

--- src/Components/Promotion/Section4/Style.ts
+++ src/Components/Promotion/Section4/Style.ts
@@ -6,14 +6,67 @@ export const Positioner = css`
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  justify-content: space-evenly;
   align-items: center;
   background-color: #f0f8ff;
   overflow: hidden;
 `;
 
+export const floating = keyframes`
+    from {
+     transform: translateY(-50%);
+     opacity: 0;
+    }
+    to{
+     transform: translateY(0);
+     opacity: 1;
+
+    }
+ `;
+
+export const MainTextWrapper = css`
+  animation-name: ${floating};
+  animation-duration: 1.5s;
+  animation-duration: leaner;
+  animation-direction: alternate;
+  animation-fill-mode: forwards;
+`;
+
+export const TextWrapper = css`
+  width: 900px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-bottom: 15%;
+`;
+
 export const MainText = css`
-  font-size: ${theme.fonts.h4};
-  font-weight: 700;
+  font-size: ${theme.fonts.h3};
+  font-weight: 800;
   text-align: center;
+`;
+export const SubText = css`
+  font-size: ${theme.fonts.h5};
+  text-align: center;
+`;
+
+export const ContainerWrapper = css`
+  width: 1250px;
+  height: 350px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const Container = css`
+  width: 570px;
+  height: 350px;
+  display: flex;
+  justify-content: space-around;
+  background-color: #fff;
+  box-shadow: 0px 32.3097px 80px #dbeeff,
+    inset 0px -8.07741px 10.7699px rgba(255, 255, 255, 0.55),
+    inset 0px -21.5398px 26.9247px #f3f3f3;
+  border-radius: 20px;
 `;
