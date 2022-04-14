eccaaf66529e05105c6d94864341468381676e69
eodn <s20063@gsm.hs.kr>
2022-04-14 10:49:43 +0900

    add : Img

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -11,3 +11,6 @@ export { default as SignUpHeaderImg } from "./SignUpHeaderImg";
 export { default as PromotionImg1 } from "./PromotionImg1";
 export { default as RoketImg } from "./RoketImg";
 export { default as PromotionManImg } from "./PromotionManImg";
+export { default as BackImg1 } from "./Promotion3BackImg/BackImg1";
+export { default as BackImg2 } from "./Promotion3BackImg/BackImg2";
+export { default as BackImg4 } from "./Promotion3BackImg/BackImg4";
