e59decf3b4cca990ac52ff36b5a0212808b85c35
JeongTaehwan <wjdxoghks11222@naver.com>
2022-05-29 17:40:24 +0900

    Update : 스타일 코드 수정

--- src/Components/Write/Style.ts
+++ src/Components/Write/Style.ts
@@ -7,9 +7,24 @@ export const Positioner = css`
   padding: 2% 12% 0;
 `;
 
+export const Input = css`
+  *:focus {
+    outline: none;
+  }
+`;
+
+export const CheckProjectContainer = css`
+  display: flex;
+  padding-top: 3%;
+
+  p {
+    margin-right: 1%;
+    font-weight: 600;
+    font-size: 18px;
+  }
+`;
+
 export const ContentsContainer = css`
-  width: 100%;
-  height: 100%;
   background: #ffffff;
   border: 0.5px solid #b5b5b5;
   box-sizing: border-box;
@@ -17,57 +32,47 @@ export const ContentsContainer = css`
   border-radius: 5px;
 `;
 
-export const Input = css`
+export const NavigationBar = css`
   width: 100%;
-  height: 100%;
-  padding: 4% 12%;
-
-  *:focus {
-    outline: none;
+  display: flex;
+  align-items: center;
+  padding: 1%;
+  Button {
+    margin-left: 1%;
   }
 `;
 
-export const NavigationBar = css`
-  display: flex;
+export const NavButtonWrapper = css`
   width: 100%;
-  height: 20%;
-  padding: 1% 0 0 0.7%;
+  display: flex;
+`;
 
-  Button {
-    margin-left: 0.5%;
-  }
+export const MarkdownWrapper = css`
+  width: 100%;
+  display: flex;
+`;
+
+export const ToolbarBlock = css``;
+
+export const FileIconInput = css`
+  display: none;
 `;
 
 export const TextArea = css`
-  width: 100%;
-  height: 1000px;
   resize: none;
-  padding: 1.3% 2%;
-
+  width: 100%;
+  height: 1200px;
   font-size: ${theme.fonts.h4};
+
   &:focus {
     outline: none;
   }
 `;
 
 export const Line = css`
-  width: 100%;
-  height: 0%;
   border: 0.5px solid #a5a5a5;
-  margin-top: 0.5%;
   opacity: 3;
 `;
-
-export const MarkdownContainer = css`
-  display: flex;
-  margin-left: 55%;
-`;
-
-export const ToolbarBlock = css`
-  margin-left: 15%;
-`;
-
-export const ToolbarItem = css``;
 
 export const Markdown = css`
   cursor: pointer;
@@ -75,23 +80,12 @@ export const Markdown = css`
   height: 25px;
 `;
 
-export const ButtonContainer = css`
-  width: 50%;
-  padding: 1% 12% 2%;
-  margin-left: 68.5%;
+export const ButtonWrapper = css`
+  display: flex;
+  float: right;
+  padding-top: 1%;
 
   Button {
-    margin-left: 1.3%;
+    margin-left: 2%;
   }
-`;
-
-export const InputPositioner = css`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  display: -webkit-flex;
-  flex-direction: column;
-  -ms-flex-direction: column;
-  justify-content: center;
-  align-items: center;
 `;
