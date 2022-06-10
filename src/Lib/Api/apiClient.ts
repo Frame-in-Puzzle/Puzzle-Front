6da81c9ffd921718681b83c90c2424659a897987
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-06-10 10:42:22 +0900

    Update 보안적 이유로 wildcard에서 url로 변경

--- src/Lib/Api/apiClient.ts
+++ src/Lib/Api/apiClient.ts
@@ -7,7 +7,8 @@ export const apiClient = axios.create({
   headers: {
     Authorization: localStorage.getItem("accessToken") ?? "",
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin":
+      "http://3.39.124.170:8080, https://server.puzzle-frame.me",
     "Access-Control-Allow-Methods": "GET,POST,DELETE,PATCH,PUT,OPTIONS",
   },
 });
