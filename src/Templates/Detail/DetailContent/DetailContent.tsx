f06cf59ec35dcf7373f8da2394c077912b580176
JeongTaehwan <wjdxoghks11222@naver.com>
2022-06-14 09:22:57 +0900

    Add : 세부조회 마크다운 문법 적용

--- src/Templates/Detail/DetailContent/DetailContent.tsx
+++ src/Templates/Detail/DetailContent/DetailContent.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import React from "react";
+import { Remark } from "react-remark";
 import * as S from "./Style";
 
 type ContentType = {
@@ -7,7 +8,11 @@ type ContentType = {
 };
 
 const DetailContent: React.FC<ContentType> = ({ contents }) => {
-  return <div css={S.Positioner}>{contents}</div>;
+  return (
+    <div css={S.Positioner}>
+      <Remark>{contents}</Remark>
+    </div>
+  );
 };
 
 export default DetailContent;
